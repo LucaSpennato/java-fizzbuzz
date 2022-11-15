cc1b7c04d6c819dced35dbdef1932912c2de6424
Luca Spennato <luca.spennato@gmail.com>
2022-11-15 17:58:11 +0100

    %15 fix in first condition

--- src/ex/fizzbuzz/FizzBuzz.java
+++ src/ex/fizzbuzz/FizzBuzz.java
@@ -6,12 +6,12 @@ public class FizzBuzz {
 		
 		for (int i = 1; i <= 100; i++) {
 					
-			if (i%5 == 0)
-				System.out.println("Fizz");
-			else if (i%3 == 0)
-				System.out.println("Buzz");
-			else if (i%15 == 0)
+			if (i%15 == 0)
 				System.out.println("FizzBuzz");
+			else if (i%3 == 0)
+				System.out.println("Fizz");
+			else if (i%5 == 0)
+				System.out.println("Bizz");
 			else
 				System.out.println(i);
 					
